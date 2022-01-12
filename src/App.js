8602ce9afd0c8672b7195c8fd9a2db59ec100f9e
BenAgilePersp <benjamin@agileperspectives.com>
2022-01-12 12:08:13 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Col, Container, Form, Row, Card, Input, Button, CardBody, CardLink } from 'reactstrap';
 import './App.css';
+import './signin.css';
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Fragment>
+        <Container className="py-5 my-5 h-100 wrap" fluid>
+
+          <h3 className="display-2 fw-bold text-uppercase col-sm-12 col-md-3 col-xl-1 d-none d-sm-block">ebxc</h3>
+          
+          <Row className='d-flex justify-content-center'>
+            <Col md={9} xl={5} xxl={6}>
+              <Card>
+                <main>
+                  <Form>
+
+                    {/*Login Message*/}
+                    <h1 class="h2 login mt-5">LOGIN</h1>
+                    <h2 class="h5 login-msg">Welcome back! Please login to your account</h2>
+
+                    <CardBody>
+                    
+                      {/*Email and Password Input*/}
+                      <Input type='email' className='form-control mb-4' id='floatingInput' placeholder='Email'/>
+                      <Input type="password" className="form-control mb-5" id="floatingPassword" placeholder="Password"/>
+
+                      {/* Remember me and Forgot Password */}
+                        <Row className='mb-4'>
+                          <Col>
+                            {/* Remember Me */}
+                            <div className='checkbox'>
+                              <label>
+                                <Input type="checkbox" value="remember-me" id="remem-me"/> Remember Me
+                              </label>
+                            </div>
+                          </Col>
+
+                          <Col>
+                            {/* Forgot Password */}
+                            <div className='link forgot-password'>
+                              Forgot Password
+                            </div>
+                          </Col>
+                        </Row>
+
+                        
+                        {/* Login Button */}
+                        <Row>
+                          <div>
+                          <Button className="w-100 btn btn-lg btn-primary mb-4" type="submit" color='primary'>Login</Button>
+                          </div>
+                        </Row>
+
+
+                        {/* Sign up */}
+                        <Row>
+                          <div className='sign-up mt-4 h6'>
+                            Don't have an account?
+                          </div>
+                          <CardLink href='#'>Sign up</CardLink>
+                        </Row>
+                    </CardBody>
+
+                    
+                  </Form>
+                </main>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </React.Fragment>
     </div>
   );
 }
